a190392a6d52723164a40774560ee61ea1da76ea
phillipsK <kjphillips@suffolk.edu>
2015-08-28 06:45:46 -0400

    Chapter 2 Case Study & .gitignore to include /out/.class

--- src/HourglassFigure.java
+++ src/HourglassFigure.java
@@ -4,37 +4,78 @@
  * Chapter 2.5
  */
 public class HourglassFigure {
+
+    public static final int SUB_HEIGHT = 4;
+
     public static void main(String[] args) {
         drawLine();
-        System.out.println();
+//            removed this by changing line 30 from print to println
+//            System.out.println();
         topHalf();
+        bottomHalf();
+        drawLine();
+        bottomHalf();
+        topHalf();
+        drawLine();
     }
 
     public static void drawLine(){
         System.out.print("+");
-            for (int i = 1;i < 7;i++){
-                System.out.print("-");
-            }
-        System.out.print("+");
+        for (int i = 1; i <= SUB_HEIGHT * 2; i++){
+            System.out.print("-");
+        }
+//            System.out.print("+");
+        System.out.println("+");
     }
 
     public static void topHalf(){
 //        total lines
-        for (int line = 1; line < 4; line++){
+        for (int line = 1; line <= SUB_HEIGHT; line++){
             System.out.print("|");
-            System.out.print("\\");
-//            spaces
+
+            //            LHS spaces
             for (int i = 1; i <= (line-1); i++){
                 System.out.print(" ");
+
             }
-//            dots
-            for (int i=1;i<=(6-(2*line));i++){
+            System.out.print("\\");
+            //            dots
+            for (int i = 1; i <= ((2 * SUB_HEIGHT) - ( 2 * line)); i++){
                 System.out.print(".");
             }
             System.out.print("/");
+//                RHS spaces
+            for (int i = 1; i <= (line-1); i++){
+                System.out.print(" ");
+
+            }
             System.out.print("|");
             System.out.println();
         }
     }
+    public static void bottomHalf(){
+//        total lines
+        for (int line = SUB_HEIGHT; line >= 1; line--){
+            System.out.print("|");
 
+            //            LHS spaces
+            for (int i = 1; i <= (line-1); i++){
+                System.out.print(" ");
+
+            }
+            System.out.print("/");
+            //            dots
+            for (int i=1; i <= ((2 * SUB_HEIGHT)-( 2 * line)); i++){
+                System.out.print(".");
+            }
+            System.out.print("\\");
+//                RHS spaces
+            for (int i = 1; i <= (line-1); i++){
+                System.out.print(" ");
+
+            }
+            System.out.print("|");
+            System.out.println();
+        }
+    }
 }
