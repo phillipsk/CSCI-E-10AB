5b6ed9f3296b4f10ddf22b77b5a952cc96869823
phillipsk <kjphillips@suffolk.edu>
2015-08-28 13:36:30 -0400

    Ch2 Q34

--- src/Ch2_SelfCheckProblems.java
+++ src/Ch2_SelfCheckProblems.java
@@ -6,19 +6,21 @@
 public class Ch2_SelfCheckProblems {
     public static void main(String[] args) {
         question34();
+        System.out.println();
+        q34();
 
     }
 
     public static void  question34(){
 /**
-* Table
-* Line 1   ! = 22  \ = 0   / = 0
-* Line 2   ! = 18  \ = 2   / = 2
-* Line 3   ! = 14  \ = 4   / = 4
-* Line 4   ! = 10  \ = 6   / = 6
-* Line 5   ! = 6   \ = 8   / = 8
-* Line 6   ! = 2   \ = 10  / = 10
-*/
+ * Table
+ * Line 1   ! = 22  \ = 0   / = 0
+ * Line 2   ! = 18  \ = 2   / = 2
+ * Line 3   ! = 14  \ = 4   / = 4
+ * Line 4   ! = 10  \ = 6   / = 6
+ * Line 5   ! = 6   \ = 8   / = 8
+ * Line 6   ! = 2   \ = 10  / = 10
+ */
 
         for (int line = 1; line <= 6; line++){
             for (int i = 1; i <= (22-2*(line-1)); i++){
@@ -31,6 +33,19 @@ public class Ch2_SelfCheckProblems {
         }
     }
 
-
+    public static void q34(){
+        for (int line = 1; line <= 6; line++){
+            for(int i = 1; i<=line-1; i++) {
+                System.out.print("\\\\");
+            }
+            for (int i = 1; i <= 22 -(4*(line-1)); i++){
+                System.out.print("!");
+            }
+            for(int i = 1; i<=line-1; i++) {
+                System.out.print("//");
+            }
+            System.out.println();
+        }
+    }
 
 }
