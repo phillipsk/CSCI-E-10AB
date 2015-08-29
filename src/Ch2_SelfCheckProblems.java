22a6cf7bc89938a5797b17c20350efb10b4148c3
phillipsK <kjphillips@suffolk.edu>
2015-08-29 06:41:32 -0400

    Ch2 Q34 Solution, scalable with constant

--- src/Ch2_SelfCheckProblems.java
+++ src/Ch2_SelfCheckProblems.java
@@ -4,22 +4,34 @@
  * Chapter 2 Self-Check Problems
  */
 public class Ch2_SelfCheckProblems {
+
+    public static final int SIZE = 4;
+
     public static void main(String[] args) {
+        System.out.println("Practice:");
         question34();
-        System.out.println();
+        System.out.println("Partially correct:");
         q34();
+        System.out.println("Solution, scalable with constant:");
+        solution34();
 
     }
 
     public static void  question34(){
 /**
- * Table
- * Line 1   ! = 22  \ = 0   / = 0
- * Line 2   ! = 18  \ = 2   / = 2
+ * Table 6 lines; CONSTANT = 6      22 -(4*(line-1)
+ * Line 1   ! = 22  \ = 0   / = 0   6 4
+ * Line 2   ! = 18  \ = 2   / = 2   4
  * Line 3   ! = 14  \ = 4   / = 4
  * Line 4   ! = 10  \ = 6   / = 6
  * Line 5   ! = 6   \ = 8   / = 8
  * Line 6   ! = 2   \ = 10  / = 10
+ *
+ * Table 4 lines; CONSTANT = 4
+ * Line 1   ! = 14  \ = 0   / = 0
+ * Line 2   ! = 10  \ = 2   / = 2
+ * Line 3   ! = 6   \ = 4   / = 4
+ * Line 4   ! = 2   \ = 6   / = 6
  */
 
         for (int line = 1; line <= 6; line++){
@@ -48,4 +60,18 @@ public class Ch2_SelfCheckProblems {
         }
     }
 
+    public static void solution34(){
+        for (int line = 1; line <= SIZE; line++){
+            for(int i = 1; i<=((2 * line) - 2); i++) {
+                System.out.print("\\");
+            }
+            for (int i = 1; i <= ( -4 * line + ( 4 * SIZE + 2 ) ); i++){
+                System.out.print("!");
+            }
+            for(int i = 1; i<= ((2 * line) - 2); i++) {
+                System.out.print("/");
+            }
+            System.out.println();
+        }
+    }
 }
