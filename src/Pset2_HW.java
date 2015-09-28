a2bd885f595fc32c582b95752b3520e2e8d94e1a
phillipsK <kjphillips@suffolk.edu>
2015-09-28 06:38:14 -0400

    Pset2

---
+++ src/Pset2_HW.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Kevin Phillips on 9/28/15.
+ * Following Reges, Stuart, and Martin Stepp. Building Java Programs: A Back to Basics Approach. 3rd Edition.
+ * Chapter
+ */
+public class Pset2_HW {
+    public static final int HEIGHT = 5;
+
+    public static void main(String[] args) {
+        for (int line = HEIGHT; line >= 1; line--){
+
+        }
+
+    }
+}
