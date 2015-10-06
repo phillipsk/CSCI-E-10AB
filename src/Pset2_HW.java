6df70ffcd820ee0e777e4aad85c37f4c67e3fbf5
phillipsK <kjphillips@suffolk.edu>
2015-10-06 08:54:21 -0400

    Pset2

--- src/Pset2_HW.java
+++ src/Pset2_HW.java
@@ -4,12 +4,33 @@
  * Chapter
  */
 public class Pset2_HW {
-    public static final int HEIGHT = 5;
+//    public static final int HEIGHT = 5;
 
     public static void main(String[] args) {
-        for (int line = HEIGHT; line >= 1; line--){
-
+/*        int x = 2;
+        System.out.println("foo (x,3) = "+ foo(x,3));
+        System.out.println("foo (x,4) = "+ foo(x, 4));
+        System.out.println("bar (-x) = "+ bar(-x));
+        System.out.println("bar (foo (x+x, ++x) ) = "+ bar(foo(x+x,++x) ));
+        System.out.println("Final value of x = " + x);*/
+    for (int outer = 2; outer <= 6; outer++){
+        for (int inner =1; inner <=8; inner++){
+            System.out.print("");
         }
+        System.out.println();
+    }
 
     }
+
+
+
+    static int foo(int a, int b){
+        return (a+b);
+    }
+
+    static int bar(int x){
+        x += 2;
+        return (x*x);
+    }
+
 }
