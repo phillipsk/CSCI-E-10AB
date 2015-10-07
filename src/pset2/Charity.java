f887996391ff0c4d997a336e2568613320f8527c
Kevin <inquiries@kevinphillips.email>
2015-10-07 13:52:18 -0400

    Charity

--- src/pset2/Charity.java
+++ src/pset2/Charity.java
@@ -10,9 +10,14 @@ public class Charity {
         Scanner console = new Scanner(System.in);
         System.out.println("Enter the amount of a contribution:  ");
         double contribution = console.nextDouble();
+//        String Response = "donor(contribution);";
+//        System.out.println("You are designated within the "+ Response + " group.");
+        System.out.println("You are designated in the group: ");
+        donor(contribution);
     }
 
-    /*  $10k or greater     = Benefactor
+    /*
+     *  $10k or greater     = Benefactor
     *   $1k <> $9,999.99    = Patrons
     *   $500 <> $999.99     = Supporters
     *   $100 <> $499.99     = Friends
@@ -20,13 +25,24 @@ public class Charity {
     *
     *   The donor method evaluates each donor's contribution and returns one of the above designations.
     *   This method passes the contribution value defined in the main method through the user's input
-     *   as a double variable defined as donation. */
+    *   as a double variable defined as donation.
+    */
     public static void donor(double donation){
 //        double benefactor = $10,000.000
         if (donation >= 10000.00){
             System.out.println("Benefactor");
             }
         else if(donation >= 1000.00 && donation <= 9999.99){
+            System.out.println("Patrons");
+        }
+        else if(donation >= 500.00 && donation <= 999.99){
+            System.out.println("Supporters");
+        }
+        else if (donation >= 100.00 && donation <= 499.99){
+            System.out.println("Friends");
+        }
+        else {
+            System.out.println("Cheapskates");
         }
     }
 }
