b9c4f633cab13419aaeb2749c81c2d8d7fca660a
Kevin <inquiries@kevinphillips.email>
2015-10-12 16:59:16 -0400

    Powers complete (comments needed)

--- src/pset2/Digits.java
+++ src/pset2/Digits.java
@@ -13,7 +13,7 @@ public class Digits {
         Scanner console = new Scanner(System.in);
 //        int number = console.nextInt();
 //        reverseNumber(console.nextInt());
-        firstDigit(console.nextInt());
+//        firstDigit(console.nextInt());
     }
 
     //    What does this method do?
@@ -26,28 +26,7 @@ public class Digits {
         return (i);
     }*/
 
-    public static int firstDigit(int n) {
-        while (n < -99999 || 99999 < n){
-            n /= 100000;
-        }
-        System.out.print(Math.abs(n));
-        while (n < -9999 || 9999 < n){
-            n /= 10000;
-        }
-        System.out.print(Math.abs(n));
-        while (n < -999 || 999 < n){
-            n /= 1000;
-        }
-        System.out.print(Math.abs(n));
-        while (n < -99 || 99 < n){
-            n /= 100;
-        }
-        System.out.print(Math.abs(n));
-        while (n < -9 || 9 < n){
-            n /= 10;
-        }
-//        return  System.out.println(Math.abs(n));
-        System.out.print(Math.abs(n));
-        return Math.abs(n);
-    }
+    /*public static int firstDigit(int n) {
+        // no while loops please
+    }*/
 }
--- src/pset2/Powers.java
+++ src/pset2/Powers.java
@@ -1,11 +1,44 @@
 package pset2;
 
+import java.util.Scanner;
+
 /**
- * Created by Kevin Phillips on 10/12/2015
- * Following Reges, Stuart, and Martin Stepp. Building Java Programs: A Back to Basics Approach. 3rd Edition.
+ * @author Kevin Phillips
+ * @since 10/12/2015
+ * CSCI-E-10a
+ * Unit 22 HW
  */
 public class Powers {
     public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.println("Please enter the maximum power: ");
+//        printPowersOf2(console.nextInt());
+        printPowersOf2(10);
+    }
 
+/*    This method calculates the powers of 2 up until the nth power, with nth represented by a user input passed
+        through main*/
+    public static void printPowersOf2(int inputPower){
+
+        int number = 1;
+        System.out.print("1 ");
+/*        for (int i = 1; i <= inputPower; i++){
+            number=number*2;
+            System.out.print(number + " ");
+        }*/
+
+        int next = 0;
+        int sum = 0;
+        for (int i = 1; i <= inputPower; i++){
+            for (int j = 0; j <= i; j++){
+                for (int k = 2; k <= j; k = k+2){
+                    next = number + i;
+                    sum += next;
+
+                }
+                System.out.print(sum + " ");
+            }
+        }
+//        System.out.print(sum + " ");
     }
 }
