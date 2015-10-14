e30f8e83195545fbafd99c2344b3d5df8249d453
Kevin <inquiries@kevinphillips.email>
2015-10-14 14:00:44 -0400

    reversed output stored in String

--- src/pset2/Digits.java
+++ src/pset2/Digits.java
@@ -18,10 +18,10 @@ public class Digits {
 //    What does this do, @param @return ???
         public static void reverseNumber(int inputNumber){
             int u = inputNumber % 10;
-            int h = inputNumber / 100;
             int t = (inputNumber % 100) / 10;
-            int th = (inputNumber % 1000) / 100;
-            int t_th = (inputNumber %10000) / 1000;
+            int h = (inputNumber % 1000) / 100;
+            int th = (inputNumber %10000) / 1000;
+            int t_th = inputNumber / 10000;
 //            debug number = 51273
             System.out.println("units = "+ u);
             System.out.println("tens = "+ t);
@@ -29,6 +29,11 @@ public class Digits {
             System.out.println("thousands = "+ th);
             System.out.println("ten thousands = "+ t_th);
 
+//            int result = u+""+ t + th + t_th + h
+//            System.out.println(u+""+ t + th + t_th + h);
+//            String inputReversed = Integer.toString(u+ t + th + t_th + h);
+            String inputReversed = u+""+ t + h + th + t_th;
+            System.out.println(inputReversed);
         }
 
 }
