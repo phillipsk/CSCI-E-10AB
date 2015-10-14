9bdc79413ac9e6aab83b50bfe81ba8b8d2f47618
Kevin <inquiries@kevinphillips.email>
2015-10-14 10:22:22 -0400

    working on h u t th t_th digits

--- src/pset2/Digits.java
+++ src/pset2/Digits.java
@@ -12,21 +12,23 @@ public class Digits {
         System.out.println("Please enter a five digit number to be reversed: ");
         Scanner console = new Scanner(System.in);
 //        int number = console.nextInt();
-//        reverseNumber(console.nextInt());
+        reverseNumber(console.nextInt());
 //        firstDigit(console.nextInt());
     }
+//    What does this do, @param @return ???
+        public static void reverseNumber(int inputNumber){
+            int u = inputNumber % 10;
+            int h = inputNumber / 100;
+            int t = (inputNumber % 100) / 10;
+            int th = (inputNumber % 1000) / 100;
+            int t_th = (inputNumber %10000) / 1000;
+//            debug number = 51273
+            System.out.println("units = "+ u);
+            System.out.println("tens = "+ t);
+            System.out.println("hundreds = "+ h);
+            System.out.println("thousands = "+ th);
+            System.out.println("ten thousands = "+ t_th);
 
-    //    What does this method do?
-    /*public static int reverseNumber(int inputNumber){
-//        illegal
-//        String s = inputNumber;
-        for (int i = 5; i <= 1; i--){
-//            System.out.print(charat);
         }
-        return (i);
-    }*/
 
-    /*public static int firstDigit(int n) {
-        // no while loops please
-    }*/
 }
