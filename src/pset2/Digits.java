eb0add7a96d3500c9e3b81a5307f9c04399a6746
Kevin <inquiries@kevinphillips.email>
2015-10-19 20:44:57 -0400

    Digits returning two four digit numbers??

--- src/pset2/Digits.java
+++ src/pset2/Digits.java
@@ -9,18 +9,30 @@ import java.util.Scanner;
  */
 public class Digits {
     public static void main(String[] args) {
-        System.out.println("Please enter a five digit number to be reversed: ");
+        /*System.out.println("Please enter a five digit number to be reversed: ");
         Scanner console = new Scanner(System.in);
-//        int number = console.nextInt();
-        int origin_INPUT = console.nextInt();
-//        reverseNumber(origin_INPUT);
-//        firstDigit(console.nextInt());
+        int origin_INPUT =  console.nextInt() * 4;
+        String origin_INPUT_REVERSED = reverseNumber(origin_INPUT);
         String origin_STRING = Integer.toString(origin_INPUT);
+//        System.out.println(origin_STRING); // debuging
+        String another_debug = Integer.toString(2178);
+        System.out.println(origin_INPUT + " " + origin_STRING + " " + origin_INPUT_REVERSED + " " + another_debug);
 
-        for (int i = 10000; i <= 99999; i++){
-            if (origin_STRING == reverseNumber(origin_INPUT)){
-//            debug number = 51273
+//        if (another_debug == origin_INPUT_REVERSED){
+        if (another_debug.equals(origin_INPUT_REVERSED)){
+            System.out.println("success");
+        }*/
+
+//        When debugging, set to four digits**
+        for (int i = 1000; i <= 9999; i++){ // SET TO FOUR
+            String for_loop_STRING = Integer.toString(i);
+            int for_loop_I_BY_FOUR = i * 4;
+            String for_loop_STRING_BY_FOUR = reverseNumber(for_loop_I_BY_FOUR);
+            if (for_loop_STRING.equals(for_loop_STRING_BY_FOUR)){
+                System.out.println(i);
             }
+
+//            System.out.println("No match");
 
         }
 
@@ -31,18 +43,19 @@ public class Digits {
             int t = (inputNumber % 100) / 10;
             int h = (inputNumber % 1000) / 100;
             int th = (inputNumber % 10000) / 1000;
-            int t_th = inputNumber / 10000;
+//            int t_th = inputNumber / 10000;
 //            debug number = 51273
-            System.out.println("units = "+ u);
+
+/*            System.out.println("units = "+ u);
             System.out.println("tens = "+ t);
             System.out.println("hundreds = "+ h);
             System.out.println("thousands = "+ th);
-            System.out.println("ten thousands = "+ t_th);
+            System.out.println("ten thousands = "+ t_th);*/
 
 //            int result = u+""+ t + th + t_th + h
 //            System.out.println(u+""+ t + th + t_th + h);
 //            String inputReversed = Integer.toString(u+ t + th + t_th + h);
-            String inputReversed = u+""+ t + h + th + t_th;
+            String inputReversed = u+""+ t + h + th; // + t_th;
 //            System.out.println(inputReversed);
             return inputReversed;
         }
