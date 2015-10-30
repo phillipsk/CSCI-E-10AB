780667f90e49b849962700b8c175dc97b6d07cef
Kevin <inquiries@kevinphillips.email>
2015-10-30 14:32:42 -0400

    pset3 working on craps.java - non robust version

--- src/pset3/craps.java
+++ src/pset3/craps.java
@@ -13,7 +13,7 @@ public class craps {
 
     public static void main(String [] args){
 
-//        diceRoll();
+        diceRoll();
     }
 
     /*public static int rInt(int a, int b){
@@ -35,15 +35,32 @@ public class craps {
 //            sum = 7;
             System.out.println("Computer rolls a " + roll1 + " and a " + roll2 + ", for a " +
                     "total of " + sum + ".");
-        } while (sum > 3 && sum < 11 && sum == 7);
-        if (COUNTER < 1){
-            System.out.println(sum + " is now the established POINT.");;
+        } while (sum >= 3 && sum <= 11 && sum == 7);
+        int point = sum;
+        System.out.println(point + " is now the established POINT.");
+        int newSUM;
+        do {
+            int roll3 = r.nextInt(6)+1;
+            int roll4 = r.nextInt(6)+1;
+            sum = roll3 + roll4;
+//            sum = 7;
+            System.out.println("Computer rolls a " + roll3 + " and a " + roll4 + ", for a " +
+                    "total of " + sum + ".");
+        }while (sum != point && sum != 7);
+        if (sum == 7){
+            System.out.println("YOU LOSE");
+        }else {
+            System.out.println("YOU WIN");
+        }
+/*        if (COUNTER < 1){
+
         }
         int point = sum;
         while (diceRoll() == point){
             System.out.println("YOU WIN");
         }
 
+*/
         return sum;
     }
 }
