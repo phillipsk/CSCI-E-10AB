c63cb5e712ebb13cb38606cd9cf108337c0c87a6
Kevin <inquiries@kevinphillips.email>
2015-11-04 10:21:16 -0500

    Ch8 point object attempt

---
+++ src/Ch8.java
@@ -0,0 +1,30 @@
+/**
+ * csci-e-10ab
+ *
+ * @author Kevin Phillips
+ * @since 11/4/2015
+ */
+public class Ch8 {
+    public class point {
+        int x;
+        int y;
+
+        // returns the distance between this point and (0,0)
+        //    ACCESSOR METHOD
+        public double distanceFromOrigin() {
+            return Math.sqrt(x * x + y * y);
+        }
+
+        public void translate(int dx, int dy) {
+            x += dx;
+            y += dy;
+        }
+    }
+
+    public static void main(String[] args){
+        Point p1 = new Point();
+        p1.x = 7;
+        p1.y = 2;
+    }
+}
+
