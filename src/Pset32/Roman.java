c6a7cd56178763f99b072e4b9e266bddd8d3fd06
phillipsK <kjphillips@suffolk.edu>
2015-11-11 21:03:58 -0500

    roman proglem and/or logic

--- src/Pset32/Roman.java
+++ src/Pset32/Roman.java
@@ -30,21 +30,28 @@ public class Roman {
 
         s = s.toUpperCase();
         int a_number = 0;
+        int aa = s.length();
         for (int i = 0; i < s.length(); i++){
-            if (s.charAt(i) == 'M'){
-                a_number+= M;
+            if (s.charAt(i) == 'M') {
+                a_number += M;
+            }else if (s.charAt(i) == 'I' && s.charAt(i) != s.length()-1 ||
+                    s.charAt(i) == 'I' && s.charAt(i+1) != 'V'){
+                    a_number += I;
             }else if (s.charAt(i) == 'D'){
                 a_number+= D;
             }else if (s.charAt(i) == 'C'){
                 a_number+= C;
-            }else if (s.charAt(i) == 'L'){
-                a_number+= L;
-            }else if (s.charAt(i) == 'X'){
+            }else if (s.charAt(i) == 'L') {
+                a_number += L;
+            }else if (s.charAt(i) == 'I' && s.charAt(i+1) == 'V'){
+                    a_number+= 4;
+            }else if (s.charAt(i) == 'V' && s.charAt(i-1) != 'I'){
+                    a_number+= V;
+            }else if (s.charAt(i) == 'I' && s.charAt(i) != s.length()-1 ||
+                    s.charAt(i) == 'I' && s.charAt(i+1) == 'X'){
+                a_number+= 9;
+            }else if (s.charAt(i) == 'X' && s.charAt(i-1) != 'I'){
                 a_number+= X;
-            }else if (s.charAt(i) == 'V'){
-                a_number+= V;
-            }else {
-                a_number+= I;
             }
         }
         return a_number;
