52681a8c709d619d9e9508ef02f9f5ead338f1ab
Kevin <inquiries@kevinphillips.email>
2015-11-25 13:55:11 -0500

    almost finished Pizza test, cost per square inch off

--- src/Pset32/PizzaTest.java
+++ src/Pset32/PizzaTest.java
@@ -8,15 +8,30 @@ package Pset32;
  */
 public class PizzaTest {
     public static void main (String [] args) {
-/*        Pizza myPizza = new Pizza ("Pepperoni", 8, 10.50, 10);
+        Pizza myPizza = new Pizza("Pepperoni", 8, 10.50, 10);
         System.out.printf ("Your %s pizza has %.2f square inches " +
                         " per slice.\n", myPizza.getName(),
                 myPizza.areaPerSlice() );
-        System.out.printf ("One slice costs $%.2f, which comes” +
-                " to $%.3f per square inch.\n",
+
+        System.out.printf ("One slice costs $%.2f, which comes to $%.3f per square inch.\n",
                 myPizza.costPerSlice(),
                 myPizza.costPerSquareInch() );
-    */
+
+        System.out.println("Your Pepperoni pizza has 20.11 square inches per slice.\n" +
+                "One slice costs $1.05, which comes to $0.052 per square inch.");
+        System.out.println();
+
+        myPizza = new Pizza("Pineapple & Pepper", 10, 11.95, 8);
+        System.out.printf ("Your %s pizza has %.2f square inches " +
+                        " per slice.\n", myPizza.getName(),
+                myPizza.areaPerSlice() );
+
+        System.out.printf ("One slice costs $%.2f, which comes to $%.3f per square inch.\n",
+                myPizza.costPerSlice(),
+                myPizza.costPerSquareInch() );
+
+        System.out.println("Your Pineapple & Pepper pizza has 39.27 square inches per slice.\n" +
+                "One slice costs $1.49, which comes to $0.038 per square inch.");
     }
 
 }
