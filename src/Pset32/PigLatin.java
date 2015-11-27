e0d190baea0bc458510b79b8f0ba80da25601dd3
Kevin <inquiries@kevinphillips.email>
2015-11-27 14:30:29 -0500

    did not complete Pig Latin need to parse sentences

--- src/Pset32/PigLatin.java
+++ src/Pset32/PigLatin.java
@@ -1,5 +1,7 @@
 package Pset32;
 
+import java.util.Scanner;
+
 /**
  * csci-e-10ab
  *
@@ -24,36 +26,45 @@ public class PigLatin {
 
     }
 
-    public static String piggie(String s){
+    public static String piggie(String s) {
 //        String
         int k = 0;
 //        for (int k = 0; k < s.length(); k++){
-            if (s.charAt(k) == 'a' || s.charAt(k) == 'A' || s.charAt(k) == 'e' || s.charAt(k) == 'E'
-            || s.charAt(k) == 'i' || s.charAt(k) == 'I' || s.charAt(k) == 'o' ||  s.charAt(k) == 'O'
-            || s.charAt(k) == 'u' || s.charAt(k) == 'U' ){
+        if (s.charAt(k) == 'a' || s.charAt(k) == 'A' || s.charAt(k) == 'e' || s.charAt(k) == 'E'
+                || s.charAt(k) == 'i' || s.charAt(k) == 'I' || s.charAt(k) == 'o' || s.charAt(k) == 'O'
+                || s.charAt(k) == 'u' || s.charAt(k) == 'U') {
 //                System.out.print(k);
 //                System.out.print(s.substring(0,s.length())+ "-" + "way ");
 //                break;
-                return s.substring(0,s.length())+ "-" + "way ";
-            }
-            else {
+            return s.substring(0, s.length()) + "-" + "way ";
+        } else {
 
-                k++;
-                if ((s.charAt(k) == 'a' || s.charAt(k) == 'A' || s.charAt(k) == 'e' || s.charAt(k) == 'E'
-                        || s.charAt(k) == 'i' || s.charAt(k) == 'I' || s.charAt(k) == 'o' ||  s.charAt(k) == 'O'
-                        || s.charAt(k) == 'y' || s.charAt(k) == 'Y')){
+            k++;
+            if ((s.charAt(k) == 'a' || s.charAt(k) == 'A' || s.charAt(k) == 'e' || s.charAt(k) == 'E'
+                    || s.charAt(k) == 'i' || s.charAt(k) == 'I' || s.charAt(k) == 'o' || s.charAt(k) == 'O'
+                    || s.charAt(k) == 'y' || s.charAt(k) == 'Y')) {
 //                    System.out.print(s.substring(1,s.length())+ "-" + s.substring(0,1) + "ay ");
 //                    break;
-                    return s.substring(1,s.length())+ "-" + s.substring(0,1) + "ay ";
-                }else {
+                return s.substring(1, s.length()) + "-" + s.substring(0, 1) + "ay ";
+            } else {
 //                    System.out.print(s.substring(2,s.length())+ "-" + s.substring(0,2) + "ay ");
 //                    break;
-                    return s.substring(2,s.length())+ "-" + s.substring(0,2) + "ay ";
-                }
+                return s.substring(2, s.length()) + "-" + s.substring(0, 2) + "ay ";
             }
+        }
+    }
+
+    public static int getInt(Scanner console, String prompt){
+        System.out.print(prompt);
+        while (!console.hasNextInt()){ //&& (console.nextInt() < 0)){
+            console.next(); // discard non-integer values as we are looking for integers only
+            System.out.println("Not an integer, Please try again.");
+            System.out.print(prompt);
+        }
+        return console.nextInt();
+    }
 //        }
 //        System.out.println();
 
 //        return s;
-    }
 }
