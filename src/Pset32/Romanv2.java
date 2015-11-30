1e826840ed22bb60226ed6656deabcad307d171c
phillipsK <kjphillips@suffolk.edu>
2015-11-30 14:18:13 -0500

    pset32 complete

--- src/Pset32/Romanv2.java
+++ src/Pset32/Romanv2.java
@@ -8,32 +8,17 @@ import java.util.Scanner;
  * @author Kevin Phillips
  * @since 11/11/2015
  */
-public class Roman {
-    /** THIS PROBLEM IS FOR EXTRA CREDIT ********
-     *
-     * This program attempts to convert Roman Numeral Syntax into Arabic equivalents.
-     * Due to a lack of time vs. complexity, the program does not completely convert all Roman Numeral syntax.
-     * Below represents my extra credit attempt.
-     */
+public class Romanv2 {
     public static void main(String[] args) {
-        System.out.println("System.out.println(romanToArabic(\"LXXXVII\"));");
         System.out.println(romanToArabic("LXXXVII"));       // 87
-        System.out.println("System.out.println(romanToArabic(\"CCXIX\"));");
         System.out.println(romanToArabic("CCXIX"));         // 219
-        System.out.println("System.out.println(romanToArabic(\"MCCCLIV\"));");
         System.out.println(romanToArabic("MCCCLIV"));       // 1354
-        System.out.println("System.out.println(romanToArabic(\"MMDCLXXIII\"));");
-        System.out.println(romanToArabic("MMDCLXXIII"));    // 2673
-        System.out.println("System.out.println(romanToArabic(\"MCDLXXVI\"));");
-        System.out.println(romanToArabic("MCDLXXVI"));      // 1476
+//        System.out.println(romanToArabic("MMDCLXXIII"));    // 2673
+//        System.out.println(romanToArabic("MCDLXXVI"));      // 1476
+//        System.out.println(romanToArabic(""));
+//        System.out.println(romanToArabic(""));
     }
 
-    /**
-     * This method evaluates each character within the Roman Numeral syntax originally passed by main.
-     *
-     * @param s represents the Roman Numeral String passed by the main method.
-     * @return  represents the Arabic equivalent to the passed parameter s
-     */
     public static int romanToArabic(String s){
         int M = 1000;
         int D = 500;
@@ -47,11 +32,15 @@ public class Roman {
         int a_number = 0;
         int aa = s.length();
         for (int i = 0; i < s.length(); i++){
-            if (s.charAt(i) == 'M') {
-                a_number += M;
-            }else if (s.charAt(i) == 'I' && s.charAt(i) != s.length()-1 ||
-                    s.charAt(i) == 'I' && s.charAt(i+1) != 'V'){
+            int bb = s.charAt(i);
+            if (s.charAt(i) == 'I' && s.charAt(i) != s.length()-1){
                 a_number += I;
+
+//            IF i = 'I' and X NOR V follow I
+            } if (s.charAt(i) == 'I' && s.charAt(i+1) != 'X' && s.charAt(i) == 'I' && s.charAt(i+1) != 'V'){
+                a_number+= I;
+            }else if(s.charAt(i) == 'M') {
+                a_number += M;
             }else if (s.charAt(i) == 'D'){
                 a_number+= D;
             }else if (s.charAt(i) == 'C'){
