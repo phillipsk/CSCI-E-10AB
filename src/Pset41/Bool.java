64734a97d0e6dcc99d8faf1ae1af9d0dc6fc0cb3
phillipsK <kjphillips@suffolk.edu>
2015-12-08 00:14:11 -0500

    completed Bool according to pset directions not yet to TA instructions

--- src/Pset41/Bool.java
+++ src/Pset41/Bool.java
@@ -10,7 +10,50 @@ package Pset41;
  * @since 12/7/15
  */
 public class Bool {
-    public static void main(String[] args) {
+    public static int fullOfBool(boolean[] list){
+        for (int i = 0; i < list.length; i++){
+            if (list[i]){
+                System.out.println("The first 'true' value is found at location " + i);
+                break;
+//                return -1;
+            }else {
+                System.out.println("There are no 'true' values.");
+            }
+        }
+//        int last = list.length - 1;
+        for (int i = list.length - 1; i >= 0; i--){
+            if (list[i]){
+                System.out.println("The last 'true' value is found at location " + i);
+                break;
+//                return -1;
+            }else {
+                System.out.println("There are no 'true' values.");
+            }
+        }
+        int counter = 0;
+        for (int i = 0; i < list.length; i++){
+            if (list[i]){
+                counter++;
+//                break;
+//                return -1;
+            }
+        }
+        System.out.println("The most consecutive 'true' value(s) are " + counter);
+        return 0;
+    }
 
+
+    public static void main(String[] args) {
+        boolean [] test1 = {false, true, true, true};
+        boolean [] test2 = {true};
+        boolean [] test3 = {true, true, true, true, false};
+        System.out.println("fullOfBool(test1);");
+        fullOfBool(test1);
+        System.out.println();
+        System.out.println("fullOfBool(test2);");
+        fullOfBool(test2);
+        System.out.println();
+        System.out.println("fullOfBool(test3);");
+        fullOfBool(test3);
     }
 }
